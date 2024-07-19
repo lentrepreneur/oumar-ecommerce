fa98de02998b76621eb87f35afb2e33e41deec6a
Diam Diallo <diamil.dev@gmail.com>
2024-07-19 21:11:45 +0000

    auth part setup done

---
+++ src/Form/RegisterFormType.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegisterFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prenom',
+                'attr' => [
+                    'class' => 'w-full outline-none p-3 border rounded-md'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrer votre prenom',
+                    ]),
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'class' => 'w-full outline-none p-3 border rounded-md'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrer votre Nom',
+                    ]),
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'class' => 'w-full outline-none p-3 border rounded-md'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrer votre email',
+                    ]),
+                    new Email([
+                        'message' => 'Mettez un email valide'
+                    ])
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => [
+                    'class' => 'w-full outline-none p-3 border rounded-md'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrer votre mot de passe',
+                    ])
+                ]
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'S\'inscrire',
+                'attr' => [
+                    'class' => 'bg-black text-white p-3 my-4'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
