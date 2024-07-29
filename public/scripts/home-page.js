169044cdf454f2ed0be915807bd57ba4941c4a70
Diam Diallo <diamil.dev@gmail.com>
2024-07-29 19:42:33 +0000

    solving javascript bugs

--- public/scripts/home-page.js
+++ public/scripts/home-page.js
@@ -1,45 +1,44 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const carousel = document.getElementById('carousel')
-  const prevButton = document.getElementById('prev-button')
-  const nextButton = document.getElementById('next-button')
-  const totalSlides = document.querySelectorAll('#carousel > div').length
+// ---Home page
+const carousel = document.getElementById('carousel')
+const prevButton = document.getElementById('prev-button')
+const nextButton = document.getElementById('next-button')
+const totalSlides = document.querySelectorAll('#carousel > div').length
 
-  let currentSlide = 0
-  let autoSlideInterval
+let currentSlide = 0
+let autoSlideInterval
 
-  function updateCarousel() {
-    const translateXValue = -currentSlide * 100 // déplacer de 100vw par slide
-    carousel.style.transform = `translateX(${translateXValue}vw)`
-  }
+function updateCarousel() {
+  const translateXValue = -currentSlide * 100 // déplacer de 100vw par slide
+  carousel.style.transform = `translateX(${translateXValue}vw)`
+}
 
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % totalSlides
-    updateCarousel()
-  }
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides
+  updateCarousel()
+}
 
-  function prevSlide() {
-    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides
-    updateCarousel()
-  }
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides
+  updateCarousel()
+}
 
-  nextButton.addEventListener('click', function () {
-    nextSlide()
-    resetAutoSlide()
-  })
-
-  prevButton.addEventListener('click', function () {
-    prevSlide()
-    resetAutoSlide()
-  })
-
-  function startAutoSlide() {
-    autoSlideInterval = setInterval(nextSlide, 5000)
-  }
-
-  function resetAutoSlide() {
-    clearInterval(autoSlideInterval)
-    startAutoSlide()
-  }
-
-  // startAutoSlide()
+nextButton.addEventListener('click', function () {
+  nextSlide()
+  resetAutoSlide()
 })
+
+prevButton.addEventListener('click', function () {
+  prevSlide()
+  resetAutoSlide()
+})
+
+function startAutoSlide() {
+  autoSlideInterval = setInterval(nextSlide, 5000)
+}
+
+function resetAutoSlide() {
+  clearInterval(autoSlideInterval)
+  startAutoSlide()
+}
+
+startAutoSlide()
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,57 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const searchButton = document.getElementById('search-button')
-  const searchOverlay = document.getElementById('overlaySearch')
+const searchButton = document.getElementById('search-button')
+const searchOverlay = document.getElementById('overlaySearch')
 
-  const menuButton = document.getElementById('menu-button')
-  const overlay = document.getElementById('overlay')
-  const closeButton = document.getElementById('close-button')
-  const body = document.body
+const menuButton = document.getElementById('menu-button')
+const overlay = document.getElementById('overlay')
+const closeButton = document.getElementById('close-button')
+const body = document.body
 
-  // searchButton.addEventListener('click', function (event) {
-  //   console.log('is clicking?')
-  //   event.stopPropagation()
-  //   mobileSearch.classList.toggle('hidden')
-  //   mobileSearch.classList.toggle('flex')
-  // })
+menuButton.addEventListener('click', () => {
+  overlay.classList.remove('hidden')
+  body.classList.add('no-scroll')
+})
 
-  menuButton.addEventListener('click', () => {
-    overlay.classList.remove('hidden')
-    body.classList.add('no-scroll')
-  })
+closeButton.addEventListener('click', () => {
+  overlay.classList.add('hidden')
+  body.classList.remove('no-scroll')
+})
 
-  closeButton.addEventListener('click', () => {
+overlay.addEventListener('click', (e) => {
+  if (e.target === overlay) {
     overlay.classList.add('hidden')
     body.classList.remove('no-scroll')
-  })
-
-  overlay.addEventListener('click', (e) => {
-    if (e.target === overlay) {
-      overlay.classList.add('hidden')
-      body.classList.remove('no-scroll')
-    }
-  })
-
-  // -----Search
-  searchButton.addEventListener('click', () => {
-    searchOverlay.classList.remove('hidden')
-    body.classList.add('no-scroll')
-  })
-
-  searchOverlay.addEventListener('click', (e) => {
-    if (e.target === searchOverlay) {
-      searchOverlay.classList.add('hidden')
-      body.classList.remove('no-scroll')
-    }
-  })
-
-  // -------------------
-  function updateValue(value) {
-    document.getElementById('rangeValue').innerText = value
   }
+})
 
-  // -----cart
+// -----Search
+searchButton.addEventListener('click', () => {
+  searchOverlay.classList.remove('hidden')
+  body.classList.add('no-scroll')
+})
 
-  // ---carousel
-
-  // ---profile update
+searchOverlay.addEventListener('click', (e) => {
+  if (e.target === searchOverlay) {
+    searchOverlay.classList.add('hidden')
+    body.classList.remove('no-scroll')
+  }
 })
